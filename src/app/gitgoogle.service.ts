4540b66a8645fe75d134ce61bebf476b178c26b9
dracangelo <kelvinmbuguaw@gmail.com>
2019-12-09 11:45:25 +0300

    modified service

--- src/app/gitgoogle.service.ts
+++ src/app/gitgoogle.service.ts
@@ -9,5 +9,14 @@ export class GitgoogleService {
   private username: string;
   private clientid ='4d67a8a6fb4d9c51e4db';
   private clientsecret = '02cf0fbc1c0991fd1d60a5a1e790751a93cf6e6a';
-  constructor(private http:HttpClient) { }
+
+  constructor(private http:HttpClient) { 
+    console.log('hello')
+    this.username = 'dracangelo';
+  }
+
+  getUserInfo(){
+    return this.http.get("http://api.github.com/users/"+ this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret);
+  }
+  
 }
