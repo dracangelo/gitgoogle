fc483bf47a97700b1526bb7bb2755c0f5f0036e6
dracangelo <kelvinmbuguaw@gmail.com>
2019-12-09 12:35:04 +0300

    modified git google html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GitgoogleComponent } from './gitgoogle/gitgoogle.component';
+import {  GitgoogleService } from './gitgoogle.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,9 +14,11 @@ import { GitgoogleComponent } from './gitgoogle/gitgoogle.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // GitgoogleService,
+    HttpClientModule ,
   ],
-  providers: [],
+  providers: [GitgoogleService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/gitgoogle/gitgoogle.component.ts
+++ src/app/gitgoogle/gitgoogle.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { GitgoogleService } from 'src/app/gitgoogle.service';
+
 @Component({
   selector: 'app-gitgoogle',
   templateUrl: './gitgoogle.component.html',
   styleUrls: ['./gitgoogle.component.css']
 })
 export class GitgoogleComponent implements OnInit {
+  user: any;
+  constructor(private GitgoogleService:GitgoogleService) {
+     this.GitgoogleService.getUserInfo().subscribe(profile => {
+       console.log(profile)
+       this.user = profile 
+     })
 
-  constructor() { }
+  }
 
   ngOnInit() {
   }
