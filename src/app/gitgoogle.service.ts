f3d8821cf7212a90be32fdd1a5395f696ef32799
dracangelo <kelvinmbuguaw@gmail.com>
2019-12-09 11:36:44 +0300

    modified service

--- src/app/gitgoogle.service.ts
+++ src/app/gitgoogle.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GitgoogleService {
 
-  constructor() { }
+  private username: string;
+  private clientid ='4d67a8a6fb4d9c51e4db';
+  private clientsecret = '02cf0fbc1c0991fd1d60a5a1e790751a93cf6e6a';
+  constructor(private http:HttpClient) { }
 }
